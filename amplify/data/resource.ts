cbe33349bfc33090b05554eb152d009fc525e554
pckk-aircon <kiyoshi.tabuchi@tk.pacific.co.jp>
2024-10-07 14:36:27 +0900

    added per-user data isolation

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -10,8 +10,7 @@ const schema = a.schema({
   Todo: a
     .model({
       content: a.string(),
-    })
-    .authorization((allow) => [allow.publicApiKey()]),
+    }).authorization(allow => [allow.owner()]),
 });
 
 export type Schema = ClientSchema<typeof schema>;
@@ -19,9 +18,9 @@ export type Schema = ClientSchema<typeof schema>;
 export const data = defineData({
   schema,
   authorizationModes: {
-    defaultAuthorizationMode: "apiKey",
-    apiKeyAuthorizationMode: {
-      expiresInDays: 30,
+    // This tells the data client in your app (generateClient())
+    // to sign API requests with the user authentication token. 
+      defaultAuthorizationMode: 'userPool',
     },
   },
 });
